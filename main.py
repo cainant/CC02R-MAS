f6131f5a0bd949c0cda1bd7de2dcca390d0bf5b3
ctravizanutto <ctravizanutto@gmail.com>
2024-11-28 17:10:59 -0300

    driver can now park

--- main.py
+++ main.py
@@ -17,37 +17,29 @@ class Parking(Environment):
         for spot_id, (city_zone, subsector_number, _) in enumerate(product(*ranges)):
             self.create(Percept("spot", (spot_id, [city_zone], subsector_number + 1, "free")))
         
-    def park_spot(self, agent, spot_number):
-        spot = self.get(Percept("spot"), (spot_number, "free"))
-        if spot:
-            self.change(spot, (spot_number, spot.args[1], spot.args[2], [agent]))
-            return "DONE"
-        else:
-            self.print(f'Cant park at spot({spot_number})')
-            return "REJECT"
+    def park_spot(self, agent, spot):
+        self.change(spot, (spot.args[0], spot.args[1], spot.args[2], [agent]))
         
     def free_spot(self, agent):
         spot = self.get(Percept("spot", ("ID", "CITY_ZONE", "SUBSECTOR", [agent])))
         if spot:
-            self.print(f'Driver {agent} is leaving spot ({spot.args[0]})')
-            self.change(spot, (spot.args[0], "free"))
+            self.print(f'({agent.str_name}) is leaving spot ({spot.args[0]})')
+            self.change(spot, (spot.args[0], spot.args[1], spot.args[2], "free"))
         else:
             self.print(f'Driver {agent} not found')
 
 class Manager(Agent):
     def __init__(self, agent_name=None):
         super().__init__(agent_name, read_all_mail=True)
+        self.prices_list = []
         self.add(Goal("CalculatePrices"))
 
     # Mocks more complex math function
     @pl(gain, Goal("CalculatePrices"))
     def calculate_prices(self, src):
-        self.spots_prices = []
-        for spot_id, (_, subsector_number, _) in enumerate(product(*ranges)):
-            price = 10 * subsector_number + rnd.random() % 5
-            self.add(Belief("price", (spot_id, price)))
-
-        # self.add(Goal("Negotiation", ("search", "north")))
+        for _, (_, subsector_number, _) in enumerate(product(*ranges)):
+            price = 10 * (subsector_number + 1) + rnd.random() % 5
+            self.prices_list.append(price)
 
     @pl(gain, Goal("Negotiation", ("ACT", "EXECUTE")))
     def negotiation(self, src, negotiation):
@@ -70,33 +62,47 @@ class Manager(Agent):
     def search_spot(self, src, search_spot):
         city_zone, agent = search_spot
         match city_zone:
-            case CityZones.N:
-                spot = self.get(Belief("spot", ("ID", [city_zone], "SUBSECTOR", "free")))
-            case CityZones.S:
-                spot = self.get(Belief("spot", ("ID", [city_zone], "SUBSECTOR", "free")))
-            case CityZones.E:
-                spot = self.get(Belief("spot", ("ID", [city_zone], "SUBSECTOR", "free")))
-            case CityZones.W:
-                spot = self.get(Belief("spot", ("ID", [city_zone], "SUBSECTOR", "free")))
+            case [CityZones.N]:
+                spot = self.get(Belief("spot", ("ID", city_zone, "SUBSECTOR", "free")))
+            case [CityZones.S]:
+                spot = self.get(Belief("spot", ("ID", city_zone, "SUBSECTOR", "free")))
+            case [CityZones.E]:
+                spot = self.get(Belief("spot", ("ID", city_zone, "SUBSECTOR", "free")))
+            case [CityZones.W]:
+                spot = self.get(Belief("spot", ("ID", city_zone, "SUBSECTOR", "free")))
         if spot:
-            self.add(Goal('SendSpot', (spot, agent)), instant=True)
+            self.add(Goal('OfferSpot', (spot, agent)), instant=True)
         return None
             
             
-
-    @pl(gain, Goal("SendSpot", ("SPOT", "AGENT")))
-    def send_spot(self, src, send_spot):
-        spot, agent = send_spot
-        self.print(f"Sending spot({spot}) to {agent}")
-        self.send(agent, achieve,Goal("park",("Parking", spot)))
-        pass
+    @pl(gain, Goal("OfferSpot", ("SPOT", "AGENT")))
+    def offer_spot(self, src, offer_spot):
+        spot, agent = offer_spot
+        spot_id = spot.args[0]
+        price = self.prices_list[spot_id]
+        self.send(agent, achieve, Goal("Negotiation", ("offer", (price, spot))))
 
 class Driver(Agent):
     def __init__(self, agent_name=None):
         super().__init__(agent_name)
         self.heading = list(CityZones)[rnd.randrange(len(CityZones.__members__))]
-        self.send("Manager", achieve, Goal("Negotiation", ("search", self.heading)))
+        self.send("Manager", achieve, Goal("Negotiation", ("search", [self.heading])))
 
+    @pl(gain, Goal("Negotiation", ("ACT", "EXECUTE")))
+    def negotiation(self, src, negotiation):
+        act, execute = negotiation
+        match act:
+            case "offer":
+                price, spot = execute
+                # check price for spot
+                if ('parking'):
+                    self.action('Parking').park_spot(self, spot)
+
+        self.stop_cycle()
+
+    def stop_cycle(self, log_flag=False) -> None:
+        self.action('Parking').free_spot(self)
+        super().stop_cycle(log_flag)
 
 if __name__ == '__main__':
     parking = Parking()
